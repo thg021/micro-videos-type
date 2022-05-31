e448a3a4cd481e74327658f7533658a9f128b0d9
Thiago Silva <itm_tsilva@uolinc.com>
2022-05-31 14:46:36 -0300

    add properties

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,3 +1,27 @@
-export default class Category {
-  constructor(public name: string){}
+export type CategoryProperties = {
+  name: string;
+  description?: string;
+  is_active?: boolean;
+  created_at?: Date;
+}
+
+export class Category {
+  constructor(public readonly props: CategoryProperties){}
+  
+  public get name() : string {
+    return this.props.name
+  }
+
+  public get description() : string  {
+    return this.props.description || ""
+  }
+  
+  public get is_active() : boolean {
+    return this.props.is_active || false
+  }
+  
+  public get created_at() : Date {
+    return this.props.created_at || new Date()
+  } 
+  
 }
