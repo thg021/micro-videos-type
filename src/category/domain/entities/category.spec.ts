13b0690d6646b1b8bc277ed2187926a4c78239a5
Thiago Silva <itm_tsilva@uolinc.com>
2022-06-01 09:08:32 -0300

    feat:
    - adicionado lodash para manipular o objeto;\n - adicionado object value unique entity id para geração de UUID; \n - Adicionado novos testes e refarado alguns pontos repetitivos no arqquivo de teste de categorias

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,8 +1,102 @@
-import Category from "./category";
-
+import UniqueEntityId from "../../../@seedwork/domain/unique-entity-id.vo"
+import { Category } from './category';
+import { omit } from 'lodash'
 describe('Category Test', () => {
   test('constructor of category', () => {
-    const category = new Category("tests")
-    expect(category.name).toBe("tests")
+
+    let category = new Category({ name: 'Movie' })
+    let props = omit(category.props, 'created_at')
+    // expect(category.name).toBe('Movie')
+    // expect(category.description).toBe('description')
+    // expect(category.is_active).toBeTruthy()
+    // expect(category.created_at).toBe(created_at)
+
+    //toMatchObject -> verificar parcialmente o objeto.
+    expect(props).toMatchObject({
+      name: 'Movie',
+      description: null,
+      is_active: true
+    })
+    expect(category.props.created_at).toBeInstanceOf(Date)
+
+    let created_at = new Date()
+    category = new Category({name: 'Movie', description: 'some description',created_at, is_active: false})
+    expect(category.props).toMatchObject({
+      name: 'Movie',
+      description: 'some description',
+      is_active: false,
+      created_at
+    })
+
+    category = new Category({name: 'Movie', description: 'other description'})
+    expect(category.props).toMatchObject({
+      name: 'Movie',
+      description: 'other description'
+    })
+
+    category = new Category({name: 'Movie', is_active: true})
+    expect(category.props).toMatchObject({
+      name: 'Movie',
+      is_active: true
+    })
+
+    created_at = new Date()
+    category = new Category({name: 'Movie', created_at})
+    expect(category.props).toMatchObject({
+      name: 'Movie',
+      created_at
+    })
+
+  });
+
+  test('id field', () => {
+
+    const data = [
+      {props: {name: 'Movie'}},
+      {props: {name: 'Movie'}, id: null},
+      {props: {name: 'Movie'}, id: undefined},
+      {props: {name: 'Movie'}, id: new UniqueEntityId()},
+    ]
+
+    data.forEach(item => {
+      const category = new Category(item.props, item?.id)
+      expect(category.id).not.toBeNull()
+      expect(category.id).toBeInstanceOf(UniqueEntityId)
+    })
+
+  });
+  test('getter of name field', () => {
+    const category = new Category({name: 'Movie'})
+    expect(category.name).toBe('Movie')
+  });
+
+  test('getter and setter of description field', () => {
+    let category = new Category({name: 'Movie', description: 'Some description'})
+    expect(category.description).toBe('Some description')
+
+    category = new Category({name: 'Movie'})
+    expect(category.description).toBeNull()
+
+    category["description"] = undefined
+    expect(category.description).toBeNull()
+  });
+
+  test('getter and setter of is_active field', () => {
+    let category = new Category({name: 'Movie'})
+    expect(category.is_active).toBeTruthy()
+
+    category = new Category({name: 'Movie', is_active: true})
+    expect(category.is_active).toBeTruthy()
+
+    category = new Category({name: 'Movie', is_active: false})
+    expect(category.is_active).toBeFalsy()
+  });
+
+  test('getter of created_at field', () => {
+    const created_at = new Date()
+    const category = new Category({name: 'Movie', created_at})
+    expect(category.created_at).toBe(created_at)
+    expect(category.created_at).toBeInstanceOf(Date)
   });
 });
+
--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,3 +1,5 @@
+import UniqueEntityId from "../../../@seedwork/domain/unique-entity-id.vo";
+
 export type CategoryProperties = {
   name: string;
   description?: string;
@@ -6,22 +8,37 @@ export type CategoryProperties = {
 }
 
 export class Category {
-  constructor(public readonly props: CategoryProperties){}
-  
-  public get name() : string {
+
+  public readonly id: UniqueEntityId;
+
+  constructor(public readonly props: CategoryProperties, id?: UniqueEntityId){
+    this.id = id || new UniqueEntityId()
+    this.description = this.props.description
+    this.is_active = this.props.is_active
+    this.props.created_at = this.props.created_at ?? new Date()
+  }
+
+  public get name() {
     return this.props.name
   }
 
-  public get description() : string  {
-    return this.props.description || ""
+  public get description(){
+    return this.props.description
   }
-  
-  public get is_active() : boolean {
-    return this.props.is_active || false
+
+  private set description(value: string){
+    this.props.description = value ?? null;
   }
-  
-  public get created_at() : Date {
-    return this.props.created_at || new Date()
-  } 
-  
+
+  public get is_active()  {
+    return this.props.is_active
+  }
+
+  private set is_active(value: boolean){
+    this.props.is_active = value ?? true
+  }
+
+  public get created_at()  {
+    return this.props.created_at
+  }
 }
