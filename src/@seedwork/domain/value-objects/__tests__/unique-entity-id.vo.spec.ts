b38af3bbd403da44708a3e2baba7f73d778ba337
Thiago Silva <itm_tsilva@uolinc.com>
2022-06-03 10:52:09 +0000

    test: testando retorno dos valores do vo

--- src/@seedwork/domain/value-objects/__tests__/unique-entity-id.vo.spec.ts
+++ src/@seedwork/domain/value-objects/__tests__/unique-entity-id.vo.spec.ts
@@ -1,5 +1,5 @@
-import InvalidUuidError from "../../../@seedwork/errors/invalid-uuid.error";
-import UniqueEntityId from "./unique-entity-id.vo";
+import InvalidUuidError from "../../../../@seedwork/errors/invalid-uuid.error";
+import UniqueEntityId from "../unique-entity-id.vo";
 import { validate as uuidValidate} from 'uuid'
 
 function spyValidateMethod(){
@@ -8,23 +8,22 @@ function spyValidateMethod(){
 describe('UniqueEntityId Unit tests', () => {
     it('should throw error when uuid is invalid', () => {
       const validateSpy = spyValidateMethod()
-      expect(() => new UniqueEntityId('fake id')).toThrow(new InvalidUuidError())
+      expect(() => new UniqueEntityId('fake id').value).toThrow(new InvalidUuidError())
       expect(validateSpy).toHaveBeenCalled()
     });
 
     it('should accept a uuid passed in constructor', () => {
       const validateSpy = spyValidateMethod()
       const uuid = '291f3061-0549-4135-b79d-63017215fdab'
-      const vo = new UniqueEntityId('291f3061-0549-4135-b79d-63017215fdab')
-      expect(() => new UniqueEntityId(uuid)).not.toThrow(new InvalidUuidError())
-      expect(vo.id).toBe(uuid)
+      const vo = new UniqueEntityId('291f3061-0549-4135-b79d-63017215fdab').value
+      expect(vo).toBe(uuid)
       expect(validateSpy).toHaveBeenCalled()
     });
 
     it('should accept a uuid passed in constructor', () => {
       const validateSpy = spyValidateMethod()
-      const vo = new UniqueEntityId()
-      expect(uuidValidate(vo.id)).toBeTruthy()
+      const vo = new UniqueEntityId().value
+      expect(uuidValidate(vo)).toBeTruthy()
       expect(validateSpy).toHaveBeenCalled()
     });
 });
---
+++ src/@seedwork/domain/value-objects/__tests__/value-object.spec.ts
@@ -0,0 +1,14 @@
+//Para class abstratas precisaremos gerar um STUB
+
+import ValueObject from "../value-object";
+
+class StubValueObject extends ValueObject{}
+describe('Value Objects test unit', () => {
+  it('should set value', () => {
+    let vo = new StubValueObject('string value')
+    expect(vo.value).toBe('string value')
+
+    vo = new StubValueObject({prop1: 'value1'})
+    expect(vo.value).toStrictEqual({prop1: 'value1'})
+  });
+});
