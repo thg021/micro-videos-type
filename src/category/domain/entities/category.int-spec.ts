6f7cba5c63ecf169c086aec091fa5df139dbcf39
Thiago Silva <itm_tsilva@uolinc.com>
2022-06-10 11:38:12 +0000

    test: teste de integracao
    adicionar teste de integracao para os metodos de create e update

--- src/category/domain/entities/category.int-spec.ts
+++ src/category/domain/entities/category.int-spec.ts
@@ -42,5 +42,62 @@ describe('Category Integration Tests', () => {
         () => new Category({ name: 'test', is_active: 'true' as any }),
       ).toThrow(new ValidatorError('The is_active must be a boolean!'))
     })
+
+    it('should a valid category', () => {
+      expect.assertions(0)
+      new Category({ name: 'Movie' })
+      new Category({ name: 'Movie', description: 'some description' })
+      new Category({ name: 'Movie', description: null })
+      new Category({
+        name: 'Movie',
+        description: 'some description',
+        is_active: true,
+      })
+      new Category({
+        name: 'Movie',
+        description: 'some description',
+        is_active: false,
+      })
+    })
+  })
+
+  describe('update method', () => {
+    it('should a throw update when create using invalid property name', () => {
+      let category = new Category({ name: 'Movie' })
+      expect(() => category.update(null)).toThrow(
+        new ValidatorError('The name is required!'),
+      )
+      expect(() => category.update('')).toThrow(
+        new ValidatorError('The name is required!'),
+      )
+
+      expect(() => category.update(2 as any)).toThrow(
+        new ValidatorError('The name must be a string!'),
+      )
+
+      expect(() => category.update('t'.repeat(256))).toThrow(
+        new ValidatorError(
+          'The name must be less or equal than 255 characters!',
+        ),
+      )
+    })
+
+    it('should a throw update when create using invalid property description', () => {
+      let category = new Category({ name: 'Movie' })
+      expect(() => category.update('test', 1 as any)).toThrow(
+        new ValidatorError('The description must be a string!'),
+      )
+
+      expect(() => category.update('test', true as any)).toThrow(
+        new ValidatorError('The description must be a string!'),
+      )
+    })
+
+    it('should a valid update', () => {
+      expect.assertions(0)
+      const category = new Category({ name: 'Movie' })
+      category.update('t'.repeat(255))
+      category.update('Movie', 'Other description')
+    })
   })
 })
