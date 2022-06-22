a6b43da59a7328598c4750674a6bc30299c44f30
Thiago Silva <thg021@gmail.com>
2022-06-22 11:25:09 +0000

    feat: in memory repository
    adicionado repositorio em memoria para testes de crud

---
+++ src/@seedwork/domain/repository/in-memory-repository.ts
@@ -0,0 +1,43 @@
+import Entity from '../entity/entity'
+import NotFoundError from '../errors/not-found-error'
+import UniqueEntityId from '../value-objects/unique-entity-id.vo'
+import { RepositoryInterface } from './repository-contracts'
+
+export default abstract class InMemoryRepository<E extends Entity>
+  implements RepositoryInterface<E>
+{
+  items: E[] = []
+
+  async insert(entity: E): Promise<void> {
+    this.items.push(entity)
+  }
+
+  async findById(id: string | UniqueEntityId): Promise<E> {
+    return this._get(`${id}`)
+  }
+
+  async findAll(): Promise<E[]> {
+    return this.items
+  }
+
+  async update(entity: E): Promise<void> {
+    await this._get(entity.id)
+    const index = this.items.findIndex((item) => item.id === entity.id)
+    this.items[index] = entity
+  }
+
+  async delete(id: string | UniqueEntityId): Promise<void> {
+    await this._get(`${id}`)
+    const index = this.items.findIndex((item) => item.id === id)
+    this.items.splice(index, 1)
+  }
+
+  protected async _get(id: string): Promise<E> {
+    const item = this.items.find((item) => item.id === id)
+
+    if (!item) {
+      throw new NotFoundError(`Entity Not Found using ID ${id}`)
+    }
+    return item
+  }
+}
