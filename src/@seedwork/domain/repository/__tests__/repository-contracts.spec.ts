b416a4c9953a2c5c31eff0fa1abee911bee604cb
Thiago Silva <thg021@gmail.com>
2022-07-21 15:33:48 +0000

    refactor: alteracao de diretorio testes
    adicionado pasta __tests___ para os repositorios

--- src/@seedwork/domain/repository/__tests__/repository-contracts.spec.ts
+++ src/@seedwork/domain/repository/__tests__/repository-contracts.spec.ts
@@ -1,4 +1,4 @@
-import { SearchParams, SearchResult } from './repository-contracts'
+import { SearchParams, SearchResult } from '../repository-contracts'
 describe('SearchParams Unit tests', () => {
   test('page props', () => {
     const arrange = [
