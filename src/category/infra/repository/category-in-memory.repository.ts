4c00082a23904d4338d72d26620f89467278e789
Thiago Silva <thg021@gmail.com>
2022-08-10 23:15:53 +0000

    feat: namaspace

--- src/category/infra/repository/category-in-memory.repository.ts
+++ src/category/infra/repository/category-in-memory.repository.ts
@@ -1,10 +1,23 @@
 import { Category } from '../../domain/entities/category'
-import {
-  InMemoryRepository,
-  InMemorySearchableRepository,
-} from '../../../@seedwork/domain/repository/in-memory-repository'
+import { InMemorySearchableRepository } from '../../../@seedwork/domain/repository/in-memory-repository'
 import CategoryRepository from '../../domain/repository/category.repository'
 
 export default class CategoryInMemoryRepository
-  extends InMemorySearchableRepository<Category>
-  implements CategoryRepository {}
+    extends InMemorySearchableRepository<Category>
+    implements CategoryRepository.Repository
+{
+    protected async applyFilter(
+        items: Category[],
+        filter: CategoryRepository.Filter,
+    ): Promise<Category[]> {
+        if (!filter) {
+            return items
+        }
+
+        return items.filter((i) => {
+            return i.props.name
+                .toLowerCase()
+                .includes(filter.toLocaleLowerCase())
+        })
+    }
+}
