60bfe26affca9c7dd42cae82a92123eedaf558a8
Thiago Silva <thg021@gmail.com>
2022-10-17 22:03:17 +0000

    feat: Providers
    organizacao dos providers

--- src/nestjs/src/categories/categories.module.ts
+++ src/nestjs/src/categories/categories.module.ts
@@ -1,59 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
-import {
-  CreateCategoryUseCase,
-  DeleteCategoryUseCase,
-  GetCategoryUseCase,
-  ListCategoriesUseCase,
-  UpdateCategoryUseCase,
-} from '@fc/code-videos/category/application';
-import { CategoryInMemoryRepository } from '@fc/code-videos/category/infra';
-import CategoryRepository from '@fc/code-videos/dist/category/domain/repository/category.repository';
+import { CATEGORIES_PROVIDERS } from './category.providers';
 
 @Module({
   controllers: [CategoriesController],
   providers: [
     CategoriesService,
-    {
-      provide: 'CategoryInMemoryRepository',
-      useClass: CategoryInMemoryRepository,
-    },
-    {
-      provide: CreateCategoryUseCase.UseCase,
-      useFactory: (categoryRepo: CategoryRepository.IRepository) => {
-        return new CreateCategoryUseCase.UseCase(categoryRepo);
-      },
-      inject: ['CategoryInMemoryRepository'],
-    },
-    {
-      provide: ListCategoriesUseCase.UseCase,
-      useFactory: (categoryRepo: CategoryRepository.IRepository) => {
-        return new ListCategoriesUseCase.UseCase(categoryRepo);
-      },
-      inject: ['CategoryInMemoryRepository'],
-    },
-    {
-      provide: GetCategoryUseCase.UseCase,
-      useFactory: (categoryRepo: CategoryRepository.IRepository) => {
-        return new GetCategoryUseCase.UseCase(categoryRepo);
-      },
-      inject: ['CategoryInMemoryRepository'],
-    },
-    {
-      provide: UpdateCategoryUseCase.UseCase,
-      useFactory: (categoryRepo: CategoryRepository.IRepository) => {
-        return new UpdateCategoryUseCase.UseCase(categoryRepo);
-      },
-      inject: ['CategoryInMemoryRepository'],
-    },
-    {
-      provide: DeleteCategoryUseCase.UseCase,
-      useFactory: (categoryRepo: CategoryRepository.IRepository) => {
-        return new DeleteCategoryUseCase.UseCase(categoryRepo);
-      },
-      inject: ['CategoryInMemoryRepository'],
-    },
+    ...Object.values(CATEGORIES_PROVIDERS.REPOSITORIES),
+    ...Object.values(CATEGORIES_PROVIDERS.USE_CASES),
   ],
 })
 export class CategoriesModule {}
---
+++ src/nestjs/src/categories/category.providers.ts
@@ -0,0 +1,61 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+import {
+  CreateCategoryUseCase,
+  DeleteCategoryUseCase,
+  GetCategoryUseCase,
+  ListCategoriesUseCase,
+  UpdateCategoryUseCase,
+} from '@fc/code-videos/category/application';
+import { CategoryInMemoryRepository } from '@fc/code-videos/category/infra';
+import CategoryRepository from '@fc/code-videos/dist/category/domain/repository/category.repository';
+
+export namespace CATEGORIES_PROVIDERS {
+  export namespace REPOSITORIES {
+    export const CATEGORY_IN_MEMORY_REPOSITORY = {
+      provide: 'CategoryInMemoryRepository',
+      useClass: CategoryInMemoryRepository,
+    };
+  }
+
+  export namespace USE_CASES {
+    export const CREATE_CATEGORY_USE_CASE = {
+      provide: CreateCategoryUseCase.UseCase,
+      useFactory: (categoryRepo: CategoryRepository.IRepository) => {
+        return new CreateCategoryUseCase.UseCase(categoryRepo);
+      },
+      inject: [REPOSITORIES.CATEGORY_IN_MEMORY_REPOSITORY.provide],
+    };
+
+    export const LIST_CATEGORY_USE_CASE = {
+      provide: ListCategoriesUseCase.UseCase,
+      useFactory: (categoryRepo: CategoryRepository.IRepository) => {
+        return new ListCategoriesUseCase.UseCase(categoryRepo);
+      },
+      inject: [REPOSITORIES.CATEGORY_IN_MEMORY_REPOSITORY.provide],
+    };
+
+    export const GET_CATEGORY_USE_CASE = {
+      provide: GetCategoryUseCase.UseCase,
+      useFactory: (categoryRepo: CategoryRepository.IRepository) => {
+        return new GetCategoryUseCase.UseCase(categoryRepo);
+      },
+      inject: [REPOSITORIES.CATEGORY_IN_MEMORY_REPOSITORY.provide],
+    };
+
+    export const UPDATE_CATEGORY_USE_CASE = {
+      provide: UpdateCategoryUseCase.UseCase,
+      useFactory: (categoryRepo: CategoryRepository.IRepository) => {
+        return new UpdateCategoryUseCase.UseCase(categoryRepo);
+      },
+      inject: [REPOSITORIES.CATEGORY_IN_MEMORY_REPOSITORY.provide],
+    };
+
+    export const DELETE_CATEGORY_USE_CASE = {
+      provide: DeleteCategoryUseCase.UseCase,
+      useFactory: (categoryRepo: CategoryRepository.IRepository) => {
+        return new DeleteCategoryUseCase.UseCase(categoryRepo);
+      },
+      inject: [REPOSITORIES.CATEGORY_IN_MEMORY_REPOSITORY.provide],
+    };
+  }
+}
