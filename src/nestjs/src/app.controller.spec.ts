01d735e9b699e8a02fd14b11048c527d2ab5fc7e
Thiago Silva <thg021@gmail.com>
2023-01-17 11:58:14 +0000

    feat: inclusao dos arquivos env para flexibilizar a alteracao de ambiente de forma dinamica

--- src/nestjs/src/app.controller.spec.ts
+++ src/nestjs/src/app.controller.spec.ts
@@ -1,6 +1,8 @@
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -9,9 +11,12 @@ describe('AppController', () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
+      imports: [ConfigModule.forRoot()],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+
+    console.log(app.get(ConfigService).get('DB_VENDOR'));
   });
 
   describe('root', () => {
