cd0163f27d11d8f0021134ef203bb166f16fb8e4
Thiago Silva <itm_tsilva@uolinc.com>
2023-01-27 03:03:28 +0000

    test: recuperar variavel de ambiente usando as tipagens

--- src/nestjs/src/app.controller.spec.ts
+++ src/nestjs/src/app.controller.spec.ts
@@ -2,26 +2,30 @@ import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from './config/config.module';
+import { ConfigModule, CONFIG_SCHEMA_TYPE } from './config/config.module';
 
 describe('AppController', () => {
-  let appController: AppController;
+    let appController: AppController;
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
-      imports: [ConfigModule.forRoot()],
-    }).compile();
+    beforeEach(async () => {
+        const app: TestingModule = await Test.createTestingModule({
+            controllers: [AppController],
+            providers: [AppService],
+            imports: [ConfigModule.forRoot()],
+        }).compile();
 
-    appController = app.get<AppController>(AppController);
+        appController = app.get<AppController>(AppController);
 
-    console.log(app.get(ConfigService).get('DB_VENDOR'));
-  });
-
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+        console.log(
+            app
+                .get<ConfigService<CONFIG_SCHEMA_TYPE>>(ConfigService)
+                .get<CONFIG_SCHEMA_TYPE['DB_VENDOR']>('DB_VENDOR'),
+        );
     });
-  });
+
+    describe('root', () => {
+        it('should return "Hello World!"', () => {
+            expect(appController.getHello()).toBe('Hello World!');
+        });
+    });
 });
