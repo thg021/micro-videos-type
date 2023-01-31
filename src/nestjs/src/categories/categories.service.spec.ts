ad3ca98c2919370db9f601d20ef7a1fbfac0b816
Thiago Silva <itm_tsilva@uolinc.com>
2023-01-31 12:18:37 +0000

    refactor: alteracao dos imports

--- src/nestjs/src/categories/categories.service.spec.ts
+++ src/nestjs/src/categories/categories.service.spec.ts
@@ -3,21 +3,21 @@ import { CategoriesService } from './categories.service';
 import { CATEGORIES_PROVIDERS } from './category.providers';
 
 describe('CategoriesService', () => {
-  let service: CategoriesService;
+    let service: CategoriesService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        CategoriesService,
-        ...Object.values(CATEGORIES_PROVIDERS.REPOSITORIES),
-        ...Object.values(CATEGORIES_PROVIDERS.USE_CASES),
-      ],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                CategoriesService,
+                ...Object.values(CATEGORIES_PROVIDERS.REPOSITORIES),
+                ...Object.values(CATEGORIES_PROVIDERS.USE_CASES),
+            ],
+        }).compile();
 
-    service = module.get<CategoriesService>(CategoriesService);
-  });
+        service = module.get<CategoriesService>(CategoriesService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 });
